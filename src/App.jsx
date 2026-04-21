8c70badd95a343f14de1c3e2fc55905ac495c12d
ASHIQANWARK <ashiqanwar777@gmail.com>
2026-04-21 17:18:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Mission from './pages/Mission';
 import Testimonials from './pages/Testimonials';
-import Team from './pages/Team';
+
 import Events from './pages/Events';
 import Inll from './pages/Inll';
 import Contacts from './pages/Contact';
@@ -28,7 +28,7 @@ function App() {
         <Route path="/inll" element={<Inll />} />
         <Route path="/mission" element={<Mission />} />
         <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/team" element={<Team />} />
+       
         <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contacts />} />
         <Route path="/donate" element={<Donate />} />
